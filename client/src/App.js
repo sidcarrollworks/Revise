5de9952c4f39bb27572919523dd277d7b405e039
squidcarroll <squidcarroll@gmail.com>
2018-05-28 02:24:02 -0700

    add homepage,loginpage,and signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Route } from 'react-router-dom';
+import './styles/App.css';
+
+import Landing from './components/landing';
+import Navbar from './components/navbar';
+import Login from './components/login';
+import Signup from './components/signup';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello im nick -______________-</h1>
+      <div className='App'>
+        <Route path="/" component={Navbar} />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
       </div>
     );
   }
