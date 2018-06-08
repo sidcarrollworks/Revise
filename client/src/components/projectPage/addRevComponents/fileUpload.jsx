10ba18011bf7ed0ea0352cd7466a25e6ee9bca2e
squidcarroll <squidcarroll@gmail.com>
2018-06-08 05:32:04 -0700

    push drop zone

--- client/src/components/projectPage/addRevComponents/fileUpload.jsx
+++ client/src/components/projectPage/addRevComponents/fileUpload.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+import '../../../styles/fileUpload.css';
 
-export default () => {
-  return (
-    <div className="fileUpload">
-      FIle upload
-    </div>
-  )
+class FileUpload extends Component {
+
+  constructor() {
+    super();
+    this.state = { files: [] }
+  }
+
+  onDrop(files) {
+    this.setState({
+      files
+    });
+  }
+
+  removeFile(e) {
+
+  }
+
+  render() {
+    return (
+      <div className="fileUpload">
+        <Dropzone className="dropzone" onDrop={this.onDrop.bind(this)}>
+          <div>
+            Drop File Here
+          </div>
+        </Dropzone>
+        <div className="droppedFiles">
+          { this.state.files.map(f => <p onCLick="" key={f.name}>{f.name} - {f.size} bytes</p>) }
+        </div>
+      </div>
+    )
+  }
 }
+
+export default FileUpload;
