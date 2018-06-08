d5d42dbe19cf854a1fc2f305de06d378106ae710
Raman Hundal <rshundal11@gmail.com>
2018-06-07 17:19:10 -0700

    Made file upload feature but still working on download

--- server/routes/api/file.js
+++ server/routes/api/file.js
@@ -1,65 +1,89 @@
+
 // env variables
-const { SECRET_ACCESS_KEY, ACCESS_KEY } = require('./config');
+const { MONGO_CONN } = require('../../../config');
 
-const express  = require('express');
-const router   = express.Router();
+const express        = require('express');
+const router         = express.Router();
 
-const multer   = require('multer');
-const aws      = require('aws-sdk');
-const multerS3 = require('multer-s3');
+const uuidv4         = require('uuid/v4');
+const multer         = require('multer');
+const path           = require('path');
+const crypto         = require('crypto');
+const GridFsStorage  = require('multer-gridfs-storage');
+const Grid           = require('gridfs-stream');
+const methodOverride = require('method-override');
 
-const mongoose = require('mongoose');
-const File     = mongoose.model('Files');
+const mongoose       = require('mongoose');
 
-// configuring aws access to aws bucket
-aws.config.update({
-  secretAccessKey: SECRET_ACCESS_KEY,
-  accessKeyId: ACCESS_KEY
+const { isAuthProj } = require('../../utils/middleware/authOnProj.js');
+
+// middleware
+router.use(methodOverride('_method'));
+
+
+// Init stream 
+let Gfs = Grid(mongoose.connection.db, mongoose.mongo);
+Gfs.collection('Uploads');
+
+// Create Storage Engine
+const storage = new GridFsStorage({
+	url: MONGO_CONN,
+	file: (req, file) => {
+		const { pid, rid } = req.params;
+		return new Promise((resolve, reject) => {
+			const fileInfo = {
+				filename: uuidv4().substring(0, 8) + "-" + file.originalname,
+				bucketName: 'Uploads',
+				chunkSize: file.chunkSize,
+				metadata: {
+					projectId: pid,
+					revisionId: rid,
+					originalFilename: file.originalname,
+				}
+			};
+			resolve(fileInfo);
+		});
+	}
 });
 
-// creating bucket endpoints
-const spacesEndpoint = new aws.Endpoint('nyc3.digitaloceanspaces.com');
-const s3 = new aws.S3({
-  endpoint: spacesEndpoint
-});
-
-const upload = multer({
-  storage: multerS3({
-    s3: s3,                                                              
-    bucket: 'revise',
-    key: (req, file, cb) => {
-
-      const fileData = {
-        name: file.originalname
-        size: file.size,
-        type: file.mimetype,
-        owner: req.user._id,
-        projId: req.params.pid,
-      }
-
-      let newFile = new File(fileData)
-
-      cb(null, Date.now().toString());
-    }
-  })
-});
+const upload = multer({ storage });
 
 
 // PUT files in their place
-
-router.put('/:pid/:rid/upload', (req, res) => {
-
+// isAuthProj
+router.put('/:pid/:rid/upload', upload.single('file'), (req, res) => {
+	console.log(req.file)
+	if (req.file)
+		res.status(200).json({success: true})
+	else
+		res.status(400).json({success: false})
 })
 
 // View and Download files
-
-router.get('/:pid/:rid/view', (req, res) => {
-
-});
-
-router.get('/:pid/:rid/download', (req, res) => {
-
+// isAuthProj
+// router.get('/:pid/:rid/download/', (req, res) => {
+router.get('/download/:fn', (req, res) => {
+	Gfs.files.findOne({ filename: req.params.fn })
+		.then(file => {
+			console.log(file)
+			if (!file || file.length === 0)
+				res.status(401);
+			console.log("beef")
+			let steam = Gfs.createReadStream({ _id: file._id })
+			console.log("after")
+			steam.pipe(res);
+				// Gfs.createReadStream({ _id: file._id }).pipe(res);
+		})
+		.catch(err => {
+			console.log("/:pid/:rid/download : ", err);
+			res.status(401);
+		})
 })
+
+// router.get('/:pid/:rid/view', isAuthProj, (req, res) => {
+
+// });
+
 
 
 module.exports = router;
